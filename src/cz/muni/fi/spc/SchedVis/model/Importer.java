fbb3d552f71576bc4662171e582acfea5c87c3ce
Lukáš Petrovický <lukas@petrovicky.net>
2008-02-29 17:38:56 +0000

    Update importer to the new dataset structure. (May need some more tweaking later.)

--- src/cz/muni/fi/spc/SchedVis/model/Importer.java
+++ src/cz/muni/fi/spc/SchedVis/model/Importer.java
@@ -14,8 +14,6 @@ import java.text.ParseException;
 import java.util.AbstractMap;
 import java.util.HashMap;
 import java.util.Iterator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * A tool to import data from specific files into the SQLite database used by
@@ -29,6 +27,15 @@ public class Importer {
 	private static SQL sql;
 	private static Integer EVENT_JOB_ARRIVAL = 1;
 	private static Integer EVENT_JOB_EXECUTION_START = 2;
+	private static Integer EVENT_JOB_CANCEL = 3;
+	private static Integer EVENT_MACHINE_RESTART = 4;
+	private static Integer EVENT_MACHINE_FAILURE = 5;
+	private static Integer EVENT_JOB_MOVE_GOOD = 6;
+	private static Integer EVENT_JOB_MOVE_BAD = 7;
+	private static Integer EVENT_MACHINE_RESTART_JOB_MOVE_GOOD = 8;
+	private static Integer EVENT_MACHINE_RESTART_JOB_MOVE_BAD = 9;
+	private static Integer EVENT_MACHINE_FAILURE_JOB_MOVE_GOOD = 10;
+	private static Integer EVENT_MACHINE_FAILURE_JOB_MOVE_BAD = 11;
 
 	/**
 	 * Parse the data set and insert its data into database.
@@ -46,8 +53,6 @@ public class Importer {
 	 */
 	private static void parseDataSet(final BufferedReader reader)
 			throws ParseException, IOException, SQLException {
-		Integer lineNo = 0;
-		Integer charNo = 0;
 		// create event types table
 		try {
 			final Statement stmt = Importer.sql.getConnection()
@@ -66,6 +71,19 @@ public class Importer {
 		eventTypes.put("job-arrival", Importer.EVENT_JOB_ARRIVAL);
 		eventTypes.put("job-execution-start",
 				Importer.EVENT_JOB_EXECUTION_START);
+		eventTypes.put("job-cancel", Importer.EVENT_JOB_CANCEL);
+		eventTypes.put("good-move", Importer.EVENT_JOB_MOVE_GOOD);
+		eventTypes.put("bad-move", Importer.EVENT_JOB_MOVE_BAD);
+		eventTypes.put("machine-failure", Importer.EVENT_MACHINE_FAILURE);
+		eventTypes.put("machine-failure-move-good",
+				Importer.EVENT_MACHINE_FAILURE_JOB_MOVE_GOOD);
+		eventTypes.put("machine-failure-move-bad",
+				Importer.EVENT_MACHINE_FAILURE_JOB_MOVE_BAD);
+		eventTypes.put("machine-restart", Importer.EVENT_MACHINE_RESTART);
+		eventTypes.put("machine-restart-move-good",
+				Importer.EVENT_MACHINE_RESTART_JOB_MOVE_GOOD);
+		eventTypes.put("machine-restart-move-bad",
+				Importer.EVENT_MACHINE_RESTART_JOB_MOVE_BAD);
 		final Iterator<String> eventTypeIterator = eventTypes.keySet()
 				.iterator();
 		while (eventTypeIterator.hasNext()) {
@@ -82,104 +100,136 @@ public class Importer {
 		try {
 			final Statement stmt = Importer.sql.getConnection()
 					.createStatement();
-			stmt
-					.executeUpdate("CREATE TABLE IF NOT EXISTS events (id_events INTEGER PRIMARY KEY AUTOINCREMENT, id_event_types INTEGER, id_machines TEXT, id_jobs INTEGER, clock FLOAT, need_cpus INTEGER, need_platform TEXT, need_ram INTEGER, need_hdd INTEGER, cpus_assigned TEXT, expect_start FLOAT, expect_end FLOAT, deadline FLOAT);");
+			stmt.executeUpdate("CREATE TABLE IF NOT EXISTS events ("
+					+ "id_events INTEGER PRIMARY KEY AUTOINCREMENT, "
+					+ "parent_id_events INTEGER, " + "id_event_types INTEGER, "
+					+ "id_machines TEXT, " + "id_machines_target TEXT, "
+					+ "id_jobs INTEGER, " + "clock INTEGER, "
+					+ "need_cpus INTEGER, " + "need_platform TEXT, "
+					+ "need_ram INTEGER, " + "need_hdd INTEGER, "
+					+ "cpus_assigned TEXT, " + "expect_start INTEGER, "
+					+ "expect_end INTEGER, " + "deadline INTEGER);");
 		} catch (final SQLException e) {
 			throw new SQLException("Error creating events' table.", e);
 		}
+		// prepare some statements
+		final PreparedStatement eventStmt = Importer.sql
+				.getConnection()
+				.prepareStatement(
+						"INSERT INTO events (id_event_types, clock, id_jobs, id_machines, id_machines_target) VALUES (?, ?, ?, ?, ?)");
+		final PreparedStatement eventDetailStmt = Importer.sql
+				.getConnection()
+				.prepareStatement(
+						"INSERT INTO events (parent_id_events, id_machines, id_jobs, need_cpus, cpus_assigned, need_platform, need_ram, need_hdd, expect_start, expect_end, deadline) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
 		// parse data set and fill the events' table
 		boolean isEOF = false;
+		Integer charCount = 0;
+		Integer eventId = 0;
 		while (!isEOF) {
-			// prepare regexes
-			final String floatRegex = "[\\d]+\\.[\\dE]+";
-			final Pattern pattern = Pattern.compile("^([\\w\\-]*)\\s("
-					+ floatRegex + ")\\s<([^|]+)(.*)>$");
 			// read and parse
 			final String line = reader.readLine();
 			if (line == null) {
 				isEOF = true;
 			} else {
-				lineNo++;
-				final Matcher matcher = pattern.matcher(line);
-				if (!matcher.matches()) {
+				final String[] parts = line.split("\t");
+				if (!eventTypes.containsKey(parts[0])) { // verify event
+					throw new ParseException("Unknown event received: "
+							+ parts[0], charCount);
+				}
+				final Integer clock = new Integer(parts[1]);
+				String affectedMachineId = "";
+				String targetMachineId = "";
+				Integer jobId = null;
+				String[] parseData = null;
+				switch (parts.length) { // verify event-specific parameters
+				case 3: // simple machine-(failure|restart) events
+					affectedMachineId = parts[2];
+					break;
+				case 7: // (.*)move-job events
+					jobId = new Integer(parts[2]);
+					affectedMachineId = parts[3];
+					targetMachineId = parts[4];
+					parseData = new String[2];
+					parseData[0] = parts[5];
+					parseData[1] = parts[6];
+					break;
+				case 4: // other known events
+					if (eventTypes.get(parts[0]).equals(Importer.EVENT_JOB_EXECUTION_START)) {
+						String strippedLine = parts[3].substring(1, parts[3].length() - 1);
+						String[] strParts = strippedLine.split(";");
+						affectedMachineId = strParts[0];
+					}
+					parseData = new String[1];
+					parseData[0] = parts[3];
+					jobId = new Integer(parts[2]);
+					break;
+				default:
 					throw new ParseException(
-							"Reached a malformatted dataset line: '" + line
-									+ "'.", charNo);
+							"Unknown parameter count for an event: "
+									+ parts.length, charCount);
 				}
-				// verify event type
-				final String eventType = matcher.group(1);
-				if (!eventTypes.containsKey(eventType)) {
-					throw new ParseException("Invalid event type " + eventType
-							+ " found.", charNo);
+				eventStmt.clearParameters();
+				eventStmt.setInt(1, eventTypes.get(parts[0]));
+				eventStmt.setInt(2, clock);
+				if (parseData != null) {
+					eventStmt.setInt(3, jobId);
 				}
-				// get some more plain data
-				final Double eventClock = new Double(matcher.group(2));
-				final String eventMachine = matcher.group(3);
-				// parse the job details
-				final String unparsedData = matcher.group(4).trim();
-				if (unparsedData.length() > 0) {
-					final PreparedStatement eventInsStmt = Importer.sql
-							.getConnection()
-							.prepareStatement(
-									"INSERT INTO events (id_event_types, id_machines, id_jobs, clock, need_cpus, need_platform, need_ram, need_hdd, cpus_assigned, expect_start, expect_end, deadline) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);");
-					// there may be multiple events
-					final String preppedData = unparsedData.substring(1); // remove
-					// leading
-					// |
-					String[] jobs;
-					if (preppedData.contains("|")) {
-						jobs = preppedData.split("\\|");
-					} else {
-						jobs = new String[1];
-						jobs[0] = preppedData;
-					}
-					for (final String jobData : jobs) {
-						if (jobData.length() == 0) {
-							continue;
-						}
-						// general parameters
-						eventInsStmt.setInt(1, eventTypes.get(eventType));
-						eventInsStmt.setString(2, eventMachine);
-						eventInsStmt.setDouble(4, eventClock);
-						// specific parameters
-						final String[] fields = jobData.split(";");
-						if (fields.length != 7) {
+				if (affectedMachineId.length() > 0) {
+					eventStmt.setString(4, affectedMachineId);
+				}
+				if (targetMachineId.length() > 0) {
+					eventStmt.setString(5, targetMachineId);
+				}
+				eventStmt.execute();
+				eventId++;
+				Integer parentEventId = eventId;
+				if (parseData != null) { // parse the parameters
+					for (String machineLine : parseData) {
+						if (!machineLine.startsWith("<")
+								|| !machineLine.endsWith(">")) {
 							throw new ParseException(
-									"Reached a malformatted dataset property line.",
-									charNo);
+									"Bad line with parameters.", charCount);
 						}
-						eventInsStmt.setInt(3, new Integer(fields[0])); // job
-						// ID
-						eventInsStmt.setInt(7, new Integer(fields[2])); // how
-						// much
-						// ram
-						// needed
-						eventInsStmt.setInt(8, new Integer(fields[3])); // how
-						// much
-						// hdd
-						// needed
-						eventInsStmt.setDouble(9, new Double(fields[4])); // expected
-						// start
-						eventInsStmt.setDouble(10, new Double(fields[5])); // expected
-						// finish
-						final Double deadline = new Double(fields[6]);
-						if (deadline >= 0) { // negative values mean no
-							// deadline
-							eventInsStmt.setDouble(11, new Double(fields[6]));
+						machineLine = machineLine.substring(1, machineLine
+								.length() - 1);
+						final String[] params = machineLine.split("\\|");
+						if (params.length == 1) {
+							// params contain only ID of a machine; do nothing
+						} else if (params.length < 1) {
+							throw new ParseException(
+									"Bad line with parameters: " + machineLine,
+									charCount);
+						} else {
+							for (Integer job = 1; job < params.length; job++) {
+								final String jobDetails[] = params[job].split(";");
+								eventDetailStmt.clearParameters();
+								eventDetailStmt.setInt(1, parentEventId);
+								eventDetailStmt.setString(2, params[0]);
+								eventDetailStmt.setInt(3,
+										new Integer(jobDetails[0]));
+								eventDetailStmt.setInt(4,
+										new Integer(jobDetails[1]));
+								eventDetailStmt.setString(5, jobDetails[2]);
+								eventDetailStmt.setString(6, jobDetails[3]);
+								eventDetailStmt.setInt(7,
+										new Integer(jobDetails[4]));
+								eventDetailStmt.setInt(8,
+										new Integer(jobDetails[5]));
+								eventDetailStmt.setInt(9,
+										new Integer(jobDetails[6]));
+								eventDetailStmt.setInt(10, new Integer(
+										jobDetails[7]));
+								if (!jobDetails[8].equals("-1")) { // deadline
+									eventDetailStmt.setInt(11, new Integer(
+											jobDetails[8]));									
+								}
+								eventDetailStmt.execute();
+								eventId++;
+							}
 						}
-						eventInsStmt.execute();
 					}
-				} else {
-					final PreparedStatement eventInsStmt = Importer.sql
-							.getConnection()
-							.prepareStatement(
-									"INSERT INTO events (id_event_types, id_machines, clock) VALUES (?, ?, ?);");
-					eventInsStmt.setInt(1, eventTypes.get(eventType));
-					eventInsStmt.setString(2, eventMachine);
-					eventInsStmt.setDouble(3, eventClock);
-					eventInsStmt.execute();
 				}
-				charNo += line.length();
+				charCount += line.length(); // take the line as parsed
 			}
 		}
 	}
@@ -205,12 +255,21 @@ public class Importer {
 		boolean isEOF = false;
 		Integer lineNo = 0;
 		Integer charNo = 0;
+		// create machine groups' table
+		try {
+			final Statement stmt = Importer.sql.getConnection()
+					.createStatement();
+			stmt
+					.executeUpdate("CREATE TABLE IF NOT EXISTS machine_groups (id_machine_groups INTEGER PRIMARY KEY, name TEXT UNIQUE);");
+		} catch (final SQLException e) {
+			throw new SQLException("Error creating machines table.", e);
+		}		
 		// create machines table
 		try {
 			final Statement stmt = Importer.sql.getConnection()
 					.createStatement();
 			stmt
-					.executeUpdate("CREATE TABLE IF NOT EXISTS machines (id_machines TEXT PRIMARY KEY, speed INTEGER, platform TEXT, os TEXT, ram INTEGER, hdd INTEGER);");
+					.executeUpdate("CREATE TABLE IF NOT EXISTS machines (id_machines TEXT PRIMARY KEY, id_machine_groups INTEGER, speed INTEGER, platform TEXT, os TEXT, ram INTEGER, hdd INTEGER);");
 		} catch (final SQLException e) {
 			throw new SQLException("Error creating machines table.", e);
 		}
