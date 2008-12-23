9d017a26c106e1d7bcccfd5554566e167e103b44
Lukáš Petrovický <lukas@petrovicky.net>
2008-12-23 14:48:22 +0000

    Making the slider buttons at least a little functional.

--- src/cz/muni/fi/spc/SchedVis/ui/SliderPanel.java
+++ src/cz/muni/fi/spc/SchedVis/ui/SliderPanel.java
@@ -5,12 +5,18 @@ package cz.muni.fi.spc.SchedVis.ui;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Calendar;
+import java.util.Date;
 
 import javax.swing.JButton;
-import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
+
+import org.apache.log4j.Logger;
 
 import cz.muni.fi.spc.SchedVis.model.entities.Event;
 import cz.muni.fi.spc.SchedVis.model.models.TimelineSliderModel;
@@ -22,14 +28,19 @@ import cz.muni.fi.spc.SchedVis.model.models.TimelineSliderModel;
  * @author Lukáš Petrovický <petrovicky@mail.muni.cz>
  * 
  */
-public class SliderPanel extends JPanel implements ChangeListener {
+public class SliderPanel extends JPanel implements ChangeListener,
+		ActionListener {
 
 	/**
 	 * 
 	 */
 	private static final long		serialVersionUID	= 6091479520934383104L;
 	private TimelineSliderModel	tlsm							= null;
-	private final JLabel				sliderLabel;
+	private final JButton				btnStart					= new JButton("|<");
+	private final JButton				btnEnd						= new JButton(">|");
+	private final JButton				btnPrev						= new JButton("<");
+	private final JButton				btnNext						= new JButton(">");
+	private static Date lastStep = new Date();
 
 	/**
 	 * 
@@ -39,9 +50,10 @@ public class SliderPanel extends JPanel implements ChangeListener {
 		// left-side buttons
 		final JPanel innerPane = new JPanel();
 		innerPane.setLayout(new FlowLayout());
-		innerPane.add(new JButton("|<"));
-		innerPane.add(new JButton("<<"));
-		innerPane.add(new JButton("<"));
+		this.btnStart.setEnabled(false);
+		this.btnPrev.setEnabled(false);
+		innerPane.add(this.btnStart);
+		innerPane.add(this.btnPrev);
 		// middle slider
 		this.add(innerPane, BorderLayout.LINE_START);
 		final TimelineSlider slider = new TimelineSlider();
@@ -50,26 +62,61 @@ public class SliderPanel extends JPanel implements ChangeListener {
 		this.add(slider, BorderLayout.CENTER);
 		// right-side buttons
 		final JPanel innerPane2 = new JPanel();
-		this.sliderLabel = new JLabel(this.getSliderDescription(slider.getValue(),
-				slider.getMaximum()));
-		innerPane2.add(this.sliderLabel, BorderLayout.LINE_END);
 		innerPane2.setLayout(new FlowLayout());
-		innerPane2.add(new JButton(">"));
-		innerPane2.add(new JButton(">>"));
-		innerPane2.add(new JButton(">|"));
+		innerPane2.add(this.btnNext);
+		innerPane2.add(this.btnEnd);
 		this.add(innerPane2, BorderLayout.LINE_END);
+		// add action listeners to buttons
+		final JButton[] buttons = new JButton[] { this.btnStart, this.btnEnd,
+				this.btnNext, this.btnPrev };
+		for (final JButton b : buttons) {
+			b.addActionListener(this);
+		}
 	}
 
-	private String getSliderDescription(final Integer value, final Integer maximum) {
-		return value + "/" + maximum;
+	@Override
+	public void actionPerformed(final ActionEvent e) {
+		Object src = e.getSource();
+		if (src.equals(this.btnEnd)) {
+			this.tlsm.setValue(this.tlsm.getMaximum());
+		} else if (src.equals(this.btnStart)) {
+			this.tlsm.setValue(this.tlsm.getMinimum());
+		} else if (src.equals(this.btnNext) || src.equals(this.btnPrev)) {
+		  Logger.getLogger(SliderPanel.class).error(e);
+			Integer step = 1;
+			if (src.equals(this.btnPrev)) {
+				step = -(step);
+			}
+			Date date = new Date(System.currentTimeMillis());
+			if ((date.getTime() - lastStep.getTime()) > 50) {
+				// next step
+				lastStep = date;
+				this.tlsm.setValue(this.tlsm.getValue() + step);
+			}
+		}
 	}
 
 	public void stateChanged(final ChangeEvent e) {
-		if (e.getSource().equals(this.tlsm)) {
-			final TimelineSliderModel tlsm = (TimelineSliderModel) e.getSource();
-			tlsm.setValue(Event.getPrevious(tlsm.getValue()).getId());
-			this.sliderLabel.setText(this.getSliderDescription(tlsm.getValue(), tlsm
-					.getMaximum()));
+		Object src = e.getSource();
+		if (src.equals(this.tlsm)) {
+			final Integer value = Event.getPrevious(this.tlsm.getValue()).getId();
+			tlsm.setValue(value);
+			if (value == this.tlsm.getMinimum()) {
+				this.btnPrev.setEnabled(false);
+				this.btnStart.setEnabled(false);
+				this.btnNext.setEnabled(true);
+				this.btnEnd.setEnabled(true);
+			} else if (value == this.tlsm.getMaximum()) {
+				this.btnPrev.setEnabled(true);
+				this.btnStart.setEnabled(true);
+				this.btnNext.setEnabled(false);
+				this.btnEnd.setEnabled(false);
+			} else {
+				this.btnPrev.setEnabled(true);
+				this.btnStart.setEnabled(true);
+				this.btnNext.setEnabled(true);
+				this.btnEnd.setEnabled(true);
+			}
 		}
 	}
 
