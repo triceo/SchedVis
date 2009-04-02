df0628be3e51c5b5bd74a666e01ca1dba8e9ad09
Lukáš Petrovický <lukas@petrovicky.net>
2009-04-02 08:21:09 +0000

    Better calculation of available processors.

--- src/cz/muni/fi/spc/SchedVis/Configuration.java
+++ src/cz/muni/fi/spc/SchedVis/Configuration.java
@@ -1,3 +1,20 @@
+/*
+    This file is part of SchedVis.
+
+    SchedVis is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    SchedVis is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with SchedVis.  If not, see <http://www.gnu.org/licenses/>.
+
+ */
 /**
  * 
  */
@@ -40,7 +57,7 @@ public class Configuration {
 
 	public static Integer getNumberOfCPUCores() {
 		return Math.max(Integer.valueOf(Configuration.getProperties().getProperty(
-		    "system.num_cores", "1")), 1);
+		    "system.num_cores", "1")), Runtime.getRuntime().availableProcessors());
 	}
 
 	protected static Properties getProperties() {
