66f2c07e92ea0eb4ca2079fe5108477123817756
Lukáš Petrovický <lukas@petrovicky.net>
2009-04-06 06:22:03 +0000

    Fix group management.

--- src/cz/muni/fi/spc/SchedVis/model/entities/MachineGroup.java
+++ src/cz/muni/fi/spc/SchedVis/model/entities/MachineGroup.java
@@ -1,18 +1,17 @@
 /*
  * This file is part of SchedVis.
  * 
- * SchedVis is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * SchedVis is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
  * 
- * SchedVis is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
+ * SchedVis is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+ * A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  * 
- * You should have received a copy of the GNU General Public License
- * along with SchedVis. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU General Public License along with
+ * SchedVis. If not, see <http://www.gnu.org/licenses/>.
  */
 /**
  * 
@@ -44,7 +43,8 @@ import cz.muni.fi.spc.SchedVis.model.Database;
  */
 @Entity
 @Cache(usage = CacheConcurrencyStrategy.NONSTRICT_READ_WRITE)
-public class MachineGroup extends BaseEntity {
+public class MachineGroup extends BaseEntity implements
+    Comparable<MachineGroup> {
 
 	/**
 	 * Get machine group with a given ID.
@@ -99,6 +99,11 @@ public class MachineGroup extends BaseEntity {
 	 */
 	public void addMachine(final Machine me) {
 		this.machines.add(me);
+	}
+
+	@Override
+	public int compareTo(final MachineGroup o) {
+		return this.getName().compareTo(o.getName());
 	}
 
 	@GeneratedValue
