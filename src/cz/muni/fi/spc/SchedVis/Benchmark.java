8b935d6c7ab41d94d12f9aec6102ff05adb7743b
Lukáš Petrovický <lukas@petrovicky.net>
2009-04-09 06:40:48 +0000

    Better benchmark.

--- src/cz/muni/fi/spc/SchedVis/Benchmark.java
+++ src/cz/muni/fi/spc/SchedVis/Benchmark.java
@@ -4,19 +4,22 @@ import java.util.Set;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import cz.muni.fi.spc.SchedVis.model.entities.Event;
 import cz.muni.fi.spc.SchedVis.model.entities.Machine;
 import cz.muni.fi.spc.SchedVis.rendering.ScheduleRenderer;
 
 public class Benchmark {
 
 	public static void run() {
+		Integer tickSpace = Event.getAllTicks().size() / 5;
 		Set<Machine> machines = Machine.getAllGroupless();
 		new ScheduleRenderer(machines.toArray(new Machine[] {})[0], 1);
 		Double totalTime = 0.0;
 		for (int i = 0; i < 10; i++) {
 			ExecutorService e = Executors.newFixedThreadPool(1);
 			Long now = System.nanoTime();
-			for (Integer clock : new Integer[] { 1, 500, 1000, 1500, 2000 }) {
+			for (Integer clock : new Integer[] { tickSpace, tickSpace * 2,
+			    tickSpace * 3, tickSpace * 4, tickSpace * 5 }) {
 				for (Machine m : machines) {
 					ScheduleRenderer mr = new ScheduleRenderer(m, clock);
 					e.submit(mr);
